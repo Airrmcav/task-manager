c415c4ece94ee12f92eda73f79552f9377dfd18c
Airrmcav <armzmcav@gmail.com>
2025-06-10 12:34:20 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://frontend-admin-trabajos.vercel.app"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://frontend-admin-trabajos.vercel.app", "https://mcav-administrador-tareas.netlify.app/"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
