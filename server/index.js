787638244f73956ce55c71a1b9e461f0ee7d47b5
Airrmcav <armzmcav@gmail.com>
2025-06-10 13:59:01 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://mcav-administrador-tareas.netlify.app/"],
+    origin: ["https://mcav-administrador-tareas.netlify.app", "http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
