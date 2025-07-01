bf7a8693f56e0d6792cad5baf1f0efc44e49e1d0
Airrmcav <armzmcav@gmail.com>
2025-07-01 10:55:37 -0600

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoutes from "./userRoute.js";
 import taskRoutes from "./taskRoute.js";
+import folderRoutes from "./folderRoute.js";
 
 const router = express.Router();
 
 router.use("/user", userRoutes);
 router.use("/task", taskRoutes);
+router.use("/folder", folderRoutes);
 
 export default router;
